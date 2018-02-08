f52c183520d7f46f9eb042b9bf977b6b9f77c6c7
KBebel <karolbebel.kb@gmail.com>
2018-02-08 07:21:39 +0100

    finaly utf-8 coding for file names, progress bar beta and downloading

--- FileUpDown.py
+++ FileUpDown.py
@@ -1,5 +1,13 @@
 import connection
 import readcred
+import patoolib
+import os
+import encodings
+import sys
+import codecs
+
+print(sys.stdin.encoding)
+
 
 Cred = readcred.ReadCredentials()
 CredentialsDic = Cred.read_credencials()
@@ -10,7 +18,36 @@ if Cred.FTP_CRED:
                                       passwd=CredentialsDic['NeltonPassword'])
 
     NeltonFTP.connect_with_ftp()
-    print(NeltonFTP.list_files())
-    NeltonFTP.cwd('_Jan')
-    print(NeltonFTP.list_files())
+
+    NeltonFTP.cwd('MWM_VPN/test/')
+
+    folder_dir = []
+    files_list_dict = []
+    NeltonFTP.list_files('withatt', folder_dir.append)
+
+    if len(folder_dir) > 1:
+        del folder_dir[0]
+        for line in folder_dir:
+            temp = line.split()
+            if temp[1] == '1':
+                files_list_dict.append({'Att': temp[0],
+                                        'Owner': temp[2],
+                                        'Group': temp[3],
+                                        'Size': temp[4],
+                                        'Date': temp[5:8],
+                                        'Name': temp[8]})
+
+
+    print(files_list_dict[0]['Name'])
+    print(NeltonFTP.size(files_list_dict[1]['Name']))
+    NeltonFTP.get(files_list_dict[1]['Name'])
+
+
+    # for entry in files_list_dict:
+    #     print('{0}\t{1}'.format(entry['Name'], entry['Att']))
+    #     if entry['Att'] == '-rw-r-----':
+    #         NeltonFTP.change_att(entry['Name'], '664')
+
+
+
     NeltonFTP.disconnect_with_ftp()
--- connection.py
+++ connection.py
@@ -1,12 +1,15 @@
 import ftplib
 import inspect
-
+import progressbar
+import encodings
 
 class Connection(ftplib.FTP):
     """connectig functions
     I've tried to inherit from FTP and adding/modifing my own functions
     but no success. Got 2 separate connections. So I've added/overwritten
-    useful for me functions"""
+    useful for me functions
+    - in init without self.host, user, passwd but with super ERROR:
+        AttributeError: 'Connection' object has no attribute 'user'"""
 
     bool_connected_with_ftp = False
     EFTP = None
@@ -23,11 +26,14 @@ class Connection(ftplib.FTP):
             try:
                 global EFTP
                 print('Connecting with {0} ..'.format(self.host), end='')
-                EFTP = ftplib.FTP(self.host, timeout=10)
+                EFTP = ftplib.FTP_TLS(self.host, timeout=10)
                 print('.Logging in..', end='')
                 EFTP.login(self.user, self.passwd)
                 self.bool_connected_with_ftp = True
                 print('.Connected.')
+                EFTP.encoding='utf-8'
+                EFTP.sendcmd('OPTS UTF8 ON')
+
             except ftplib.all_errors as e:
                 print(str(e))
 
@@ -38,7 +44,7 @@ class Connection(ftplib.FTP):
             print('.Connection closed.')
             self.bool_connected_with_ftp = False
 
-    def is_connected(self, name='no name'):
+    def is_connected(self, name=''):
         if self.bool_connected_with_ftp:
             try:
                 EFTP.voidcmd('NOOP')
@@ -51,10 +57,10 @@ class Connection(ftplib.FTP):
             print("function: \"{0}\" can\'t run: \n\tnot connected with {1}"
                   .format(name, str(self.host)))
 
-    def list_files(self, withatt="NO"):
+    def list_files(self, withatt="withoutatt", callback='None'):
         if self.is_connected(inspect.stack()[0][3]):
-            if withatt.upper() == 'YES':
-                return EFTP.retrlines('LIST')
+            if withatt.lower() == 'withatt':
+                return EFTP.retrlines('LIST', callback)
             else:
                 return EFTP.nlst()
 
@@ -62,7 +68,7 @@ class Connection(ftplib.FTP):
         # return tuple of allowed commands
         pass
 
-    def send_command(self, command='NOOP', type='None'):
+    def sendcmd(self, cmd='NOOP', type='None'):
         # Commands Home.pl:
         # ABOR    ACCT*   ADAT*   ALLO    APPE    AUTH    CCC *   CDUP
         # CONF*   CWD     DELE    ENC *   EPRT    EPSV    FEAT    HELP
@@ -72,7 +78,7 @@ class Connection(ftplib.FTP):
         # RNTO    SITE    SIZE    SMNT*   STAT*   STOR    STOU*   STRU
         # SYST    TYPE    USER    XCUP    XCWD    XMKD    XPWD    XRMD
         # ftp.retrlines('LIST')!!!!
-        self.cmnd = command
+        self.cmnd = cmd
         self.type = type
         if self.is_connected(inspect.stack()[0][3]):
             try:
@@ -97,5 +103,41 @@ class Connection(ftplib.FTP):
                 print('Error: "{3}"\n\twhen changing {0} to {1}'
                       .format(str(att), str(filename, e)))
 
+    def check_att(self, filename):
+        pass
+
     def cwd(self, directory):
-        EFTP.voidcmd('CWD ' + directory)
+        # EFTP.voidcmd('CWD ' + directory)
+        EFTP.cwd(directory)
+
+    def mlsd(self, path="", facts=[]):
+        EFTP.mlsd(path="", facts=[])
+
+    def retrlines(self, cmd, callback = None):
+        EFTP.retrlines(cmd, callback = None)
+
+    def get(self, filename, delete='no'):
+
+        # gfile = open(filename, "wb")
+        filesize = self.size(filename)
+        progress = progressbar.AnimatedProgressBar(end=filesize, width=50)
+        with open(filename, 'wb') as f:
+            def callback(chunk):
+                f.write(chunk)
+                progress + len(chunk)
+                progress.show_progress()
+
+            EFTP.retrbinary('RETR ' + filename, callback, 1024)
+        # gfile.close()
+
+
+
+        if delete.lower() == 'delete':
+            EFTP.delete(filename)
+
+    def size(self, filename):
+        return EFTP.size(filename)
+
+
+
+
